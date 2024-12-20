2e81e145903088b6c57821ec82287b3b5a529f0b
rezaahmadzadeh1377 <reza.ahmadzadeh1377@gmail.com>
2024-12-20 15:00:53 +0330

    Update streamlit_snappfood.py

--- streamlit_snappfood.py
+++ streamlit_snappfood.py
@@ -61,20 +61,20 @@ elif not region:
 else:
     filtered_df = df3[df3["region"].isin(region) & df3["fruit type"].isin(fruit_type)]
 
-st.subheader("Region wise Sales")
-fig = px.bar(filtered_df, y= "amount", x = "dates")
-st.plotly_chart(fig,use_container_width=True)  
+#st.subheader("Region wise Sales")
+#fig = px.bar(filtered_df, y= "amount", x = "dates")
+#st.plotly_chart(fig,use_container_width=True)  
 
 coll1, coll2 = st.columns((2))
 
-with col11:
+with coll1:
     st.subheader("Relationship between real price and amount")
     fig1 = px.scatter(filtered_df, y = "amount", x= "real price",trendline='ols',log_x=True,log_y=True)
     
     st.plotly_chart(fig1,use_container_width=True)
 
 
-with col12:
+with coll2:
     results = px.get_trendline_results(fig1)
     st.write(results.px_fit_results.iloc[0].summary())
     
