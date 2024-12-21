765eba1633d29e68732d6e9aeb1e2dc6af044269
rezaahmadzadeh1377 <reza.ahmadzadeh1377@gmail.com>
2024-12-21 14:02:37 +0330

    Update streamlit_snappfood.py

--- streamlit_snappfood.py
+++ streamlit_snappfood.py
@@ -143,7 +143,7 @@ sales_cycle, sales_trend = hpfilter(filtered_df['amount'], lamb=1600)
 st.subheader("Relationship between real price and amount")
 filtered_df['cycle'] = sales_cycle
 filtered_df['trend'] = sales_trend
-fig1 = px.line(filtered_df, y = [""trend"], x = "dates")
+fig1 = px.line(filtered_df, y = ["trend"], x = "dates")
     
 st.plotly_chart(fig1,use_container_width=True)
 
