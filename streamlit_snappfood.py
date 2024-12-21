27aaa355fc8af23cc6e7d2f48c25fe21049b2484
rezaahmadzadeh1377 <reza.ahmadzadeh1377@gmail.com>
2024-12-21 14:10:52 +0330

    Update streamlit_snappfood.py

--- streamlit_snappfood.py
+++ streamlit_snappfood.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="fruitstore!!!", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample fruitstore")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
 
 fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
 
